d8ed6078cbaa5ff3eb16f30b67a66ed314db31c5
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-11-18 10:35:39 +0800

    Changed to Vision system

--- CoreMLTest/CoreMLTest/ViewModel/ViewModel.swift
+++ CoreMLTest/CoreMLTest/ViewModel/ViewModel.swift
@@ -8,20 +8,43 @@
 
 import UIKit
 import CoreML
+import Vision
 
 class ViewModel: NSObject {
     
     lazy var inception: Inceptionv3 = Inceptionv3()
     
-    func classify(image: UIImage) -> String {
-        let buffer = CoreMLHelper.pixelBufferFromImage(image: image)
-        do {
-            let answer = try inception.prediction(image: buffer)
-            return answer.classLabel
-        } catch {
-            print(error)
+    func classify(image: UIImage) {
+        
+        guard let visionCoreMLModel = try? VNCoreMLModel(for: inception.model) else {
+            fatalError("Unable to convert to Vision Core ML Model")
         }
-        return "ops, not a match"
+        
+        let classificationRequest = VNCoreMLRequest(model: visionCoreMLModel, completionHandler: self.handleClassificationResults)
+        
+        guard let cgImage = image.cgImage else {
+            fatalError("Unable to convert \(image) to CGImage.")
+        }
+        let cgImageOrientation = CGImagePropertyOrientation(rawValue: UInt32(image.imageOrientation.rawValue))!
+        let handler = VNImageRequestHandler(cgImage: cgImage, orientation: cgImageOrientation)
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try handler.perform([classificationRequest])
+            } catch {
+                print("Error performing food classification")
+            }
+        }
+    }
+    
+    private func handleClassificationResults(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let classifications = request.results as? [VNClassificationObservation], let topClassification = classifications.first else {
+                    return
+            }
+            
+            print(topClassification.identifier)
+        }
     }
     
 }
